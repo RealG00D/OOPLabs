d9b5fef14f0944f7a6a320527d7796cf66664f38
RealG00D <oderevetckyI@gmail.com>
2024-02-14 17:39:40 +0200

    Lab1 change

--- lab1/lab1/src/Main.java
+++ lab1/lab1/src/Main.java
@@ -3,45 +3,55 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        // Введення початкових значень
-        double a = getInput("Введіть значення для a: ");
-        double b = getInput("Введіть значення для b: ");
-        double x = getInput("Введіть значення для x: ");
+class Calculator {
+    private double a;
+    private double b;
+    private double x;
 
-        double y;
-        double z;
+    public Calculator(double a, double b, double x) {
+        this.a = a;
+        this.b = b;
+        this.x = x;
+    }
 
-        // Обчислення для y
-        y = Math.pow(Math.sin(Math.pow((x * x + a), 2)), 3) - Math.sqrt(x / b);
+    public double calculateY() {
+        return Math.pow(Math.sin(Math.pow((x * x + a), 2)), 3) - Math.sqrt(x / b);
+    }
 
-        // Виведення значень вхідних даних та результату для y
+    public double calculateZ() {
+        return (x * x) / a + Math.cos(Math.pow((x + b), 3));
+    }
+
+    public void printResults() {
         System.out.println("\nОбчислення для Y:");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println("x = " + x);
-        System.out.println("Y = " + y);
+        System.out.println("Y = " + calculateY());
 
-        // Обчислення для z
-        z = (x * x) / a + Math.cos(Math.pow((x + b), 3));
-
-        // Виведення значень вхідних даних та результату для z
         System.out.println("\nОбчислення для Z:");
-        System.out.println("Z = " + z);
+        System.out.println("Z = " + calculateZ());
+    }
+}
 
-        // Виведення поточної дати і часу
+public class Main {
+    public static void main(String[] args) {
+        double a = getInput("Введіть значення для a: ");
+        double b = getInput("Введіть значення для b: ");
+        double x = getInput("Введіть значення для x: ");
+
+        Calculator calculator = new Calculator(a, b, x);
+        calculator.printResults();
+
         printCurrentDateTime();
     }
 
-    // Метод для введення значень з клавіатури
     private static double getInput(String message) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         System.out.print(message);
         return scanner.nextDouble();
     }
 
-    // Метод для виведення поточної дати і часу
     private static void printCurrentDateTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE dd MMMM");
         Date currentDate = new Date();
