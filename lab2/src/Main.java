943c73248dd918089fcc7a49ea0e3384437253c7
RealG00D <oderevetckyI@gmail.com>
2024-03-28 10:16:33 +0200

    lab5 done

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -2,10 +2,16 @@ public class Main {
     private static Phone[] phoneArray = new Phone[5];
 
     private static void createPhoneArray() {
-        phoneArray[0] = new Phone(1, "Ivanov", "Ivan", "Ivanovich", "123456789", 50, 20);
-        phoneArray[1] = new Phone(2, "Koval", "Nazar", "Ivanovich", "11241414", 29, 100);
-        phoneArray[2] = new Phone(3, "Derevetskyi", "Oleh", "Ivanovich", "543421311", 80, 56);
+        phoneArray[0] = new Phone();
+        phoneArray[1] = new Phone("Ivanovich", "11241414", 29, 100);
+        phoneArray[2] = new Phone(3, "Derevetskyi", "Oleh");
+        phoneArray[3] = new Phone(3, "Derevetskyi", "Oleh", "Ivanovich", "543421311", 80, 56);
+        phoneArray[4] = new Phone(phoneArray[0]);
 
+    }
+    private static void filterByCityCallDuration(){
+        System.out.println("Абоненти з міським часом розмов більше 30 хвилин:");
+        filterByCityCallDuration(30);
     }
 
     private static void filterByCityCallDuration(int duration) {
@@ -25,7 +31,10 @@ public class Main {
         }
     }
 
-
+    private static void filterByAccountNumber() {
+        System.out.println("Абоненти з номером рахунку '123456789':");
+        filterByAccountNumber("123456789");
+    }
 
     private static void filterByAccountNumber(String accountNumber) {
         for (Phone phone : phoneArray) {
@@ -39,13 +48,17 @@ public class Main {
     public static void main(String[] args) {
         createPhoneArray();
 
-        System.out.println("Абоненти з міським часом розмов більше 30 хвилин:");
-        filterByCityCallDuration(30);
+        for(Phone firstName: phoneArray){
+            System.out.println(firstName);
+        }
 
-        System.out.println("Абоненти, які користувались міжміським зв'язком:");
-        filterByIntercityCalls();
+//        System.out.println("Абоненти з міським часом розмов більше 30 хвилин:");
+//         filterByCityCallDuration(30);
 
-        System.out.println("Абоненти з номером рахунку '123456789':");
-        filterByAccountNumber("123456789");
+        //  System.out.println("Абоненти, які користувались міжміським зв'язком:");
+        // filterByIntercityCalls();
+
+        //  System.out.println("Абоненти з номером рахунку '123456789':");
+        //filterByAccountNumber("123456789");
     }
 }
--- lab2/src/Phone.java
+++ lab2/src/Phone.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.css.Counter;
+
 public class Phone {
     private long id;
     private String lastName;
@@ -6,9 +8,35 @@ public class Phone {
     private String accountNumber;
     private Integer cityCallDuration;
     private Integer intercityCallDuration;
+    public static int counter = 0;
+    public static int getCounter(){
+        return counter;
+    }
+
+
+    public Phone() {
+        this.id = ++counter;
+        this.lastName = "Ivanov";
+        this.firstName = "Ivan";
+        this.patronymic = "Ivanovich";
+        this.accountNumber = "123456789";
+        this.cityCallDuration = 50;
+        this.intercityCallDuration = 20;
+    }
+
+    public Phone(Phone other) {
+        this.id = ++counter;
+        this.lastName = other.lastName;
+        this.firstName = other.firstName;
+        this.patronymic = other.patronymic;
+        this.accountNumber = other.accountNumber;
+        this.cityCallDuration = other.cityCallDuration;
+        this.intercityCallDuration = other.intercityCallDuration;
+    }
 
     public Phone(long id, String lastName, String firstName, String patronymic, String accountNumber,
                  Integer cityCallDuration, Integer intercityCallDuration) {
+        counter++;
         this.id = id;
         this.lastName = lastName;
         this.firstName = firstName;
@@ -16,6 +44,28 @@ public class Phone {
         this.accountNumber = accountNumber;
         this.cityCallDuration = cityCallDuration;
         this.intercityCallDuration = intercityCallDuration;
+    }
+
+    public Phone(String patronymic, String accountNumber,
+                 Integer cityCallDuration, Integer intercityCallDuration) {
+        this.id = ++counter;
+        this.lastName = "Ivanov";
+        this.firstName = "Ivan";
+        this.patronymic = patronymic;
+        this.accountNumber = accountNumber;
+        this.cityCallDuration = cityCallDuration;
+        this.intercityCallDuration = intercityCallDuration;
+    }
+
+    public Phone(long id, String lastName, String firstName) {
+        counter++;
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.patronymic = "Ivanovich";
+        this.accountNumber = "123456789";
+        this.cityCallDuration = 50;
+        this.intercityCallDuration = 20;
     }
 
     public long getId() {
